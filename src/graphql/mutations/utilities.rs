83c4e4cc731178dc20dae56ac9b0965d8b4ea4d6
Nick Senger <nicholas.senger@synthego.com>
2020-04-23 22:12:56 -0700

    chore: refactor to custom error type

--- src/graphql/mutations/utilities.rs
+++ src/graphql/mutations/utilities.rs
@@ -10,11 +10,18 @@ use std::{
   path::Path,
 };
 
+#[derive(Debug)]
+pub enum TRCError {
+  Request(reqwest::Error),
+  FileSystem(std::io::Error),
+  Unknown(String),
+}
+
 pub fn get_image_from_google(
   language_abbr: &str,
   word: &str,
   sanitized: &str,
-) -> Result<(), String> {
+) -> Result<(), TRCError> {
   if Path::new(&format!(
     "./static/images/{}/{}.jpg",
     language_abbr, sanitized
@@ -28,32 +35,35 @@ pub fn get_image_from_google(
     "https://www.google.com/search?q={}&tbm=isch&tbs=ift:jpg",
     word
   ))
-  .map_err(|err| err.to_string())?;
+  .map_err(TRCError::Request)?;
 
-  let text = search_response.text().map_err(|err| err.to_string())?;
+  let text = search_response.text().map_err(TRCError::Request)?;
   let mut source = Document::from_read(::std::io::Cursor::new(text.into_bytes()))
-    .map_err(|err| err.to_string())
+    .map_err(TRCError::FileSystem)
     .and_then(|document| {
-      document.find(And(
-        Attr("style", "border:1px solid #ccc;padding:1px"),
-        Name("img"),
-      ))
-      .filter_map(|n| n.attr("src"))
-      .nth(0)
-      .ok_or("No image found in document!".to_owned())
-      .and_then(|url| reqwest::get(url).map_err(|err| err.to_string()))
+      document
+        .find(And(
+          Attr("style", "border:1px solid #ccc;padding:1px"),
+          Name("img"),
+        ))
+        .filter_map(|n| n.attr("src"))
+        .nth(0)
+        .ok_or(TRCError::Unknown(
+          "Failed to find first image in document".to_owned(),
+        ))
+        .and_then(|url| reqwest::get(url).map_err(TRCError::Request))
     })?;
-  
   if !Path::new(&format!("./static/images/{}", language_abbr)).exists() {
-    create_dir(&format!("./static/images/{}", language_abbr)).map_err(|err| err.to_string())?;
+    create_dir(&format!("./static/images/{}", language_abbr)).map_err(TRCError::FileSystem)?;
   }
 
   let mut dest = File::create(format!(
     "./static/images/{}/{}.jpg",
     language_abbr, sanitized
-  )).map_err(|err| err.to_string())?;
+  ))
+  .map_err(TRCError::FileSystem)?;
 
-  copy(&mut source, &mut dest).map_err(|err| err.to_string())?;
+  copy(&mut source, &mut dest).map_err(TRCError::FileSystem)?;
 
   Ok(())
 }
@@ -62,7 +72,7 @@ pub fn get_audio_from_google(
   language_abbr: &str,
   word: &str,
   sanitized: &str,
-) -> Result<(), String> {
+) -> Result<(), TRCError> {
   if Path::new(&format!(
     "./static/audio/{}/{}.mp3",
     language_abbr, sanitized
@@ -73,18 +83,19 @@ pub fn get_audio_from_google(
   }
 
   let url = google_translate_tts::url(word, language_abbr);
-  let mut source = reqwest::get(&url).map_err(|err| err.to_string())?;
+  let mut source = reqwest::get(&url).map_err(TRCError::Request)?;
 
   if !Path::new(&format!("./static/audio/{}", language_abbr)).exists() {
-    create_dir(&format!("./static/audio/{}", language_abbr)).map_err(|err| err.to_string())?;
+    create_dir(&format!("./static/audio/{}", language_abbr)).map_err(TRCError::FileSystem)?;
   }
 
   let mut dest = File::create(format!(
     "./static/audio/{}/{}.mp3",
     language_abbr, sanitized
-  )).map_err(|err| err.to_string())?;
+  ))
+  .map_err(TRCError::FileSystem)?;
 
-  copy(&mut source, &mut dest).map_err(|err| err.to_string())?;
+  copy(&mut source, &mut dest).map_err(TRCError::FileSystem)?;
 
   Ok(())
 }
