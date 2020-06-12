66a4268863b326cebfbbcef1426dacc9ebfc34a2
Nick Senger <nicholas.senger@synthego.com>
2020-06-11 17:39:57 -0700

    fix: image scraping

--- src/graphql/mutations/utilities.rs
+++ src/graphql/mutations/utilities.rs
@@ -1,90 +1,87 @@
+use crate::TRCError;
 use google_translate_tts;
 use reqwest;
 use select::{
-  document::Document,
-  predicate::{And, Attr, Name},
+    document::Document,
+    predicate::{And, Attr, Name},
 };
 use std::{
-  fs::{create_dir, File},
-  io::copy,
-  path::Path,
+    fs::{create_dir, File},
+    io::copy,
+    path::Path,
 };
-use crate::TRCError;
 
 pub fn get_image_from_google(
-  language_abbr: &str,
-  word: &str,
-  sanitized: &str,
+    language_abbr: &str,
+    word: &str,
+    sanitized: &str,
 ) -> Result<(), TRCError> {
-  if Path::new(&format!(
-    "./static/images/{}/{}.jpg",
-    language_abbr, sanitized
-  ))
-  .exists()
-  {
-    return Ok(());
-  }
-
-  let mut search_response = reqwest::get(&format!(
-    "https://www.google.com/search?q={}&tbm=isch&tbs=ift:jpg",
-    word
-  ))?;
-
-  let text = search_response.text()?;
-  let document = Document::from_read(::std::io::Cursor::new(text.into_bytes()))?;
-  let url = document
-    .find(And(
-      Attr("style", "border:1px solid #ccc;padding:1px"),
-      Name("img"),
+    if Path::new(&format!(
+        "./static/images/{}/{}.jpg",
+        language_abbr, sanitized
     ))
-    .filter_map(|n| n.attr("src"))
-    .nth(0)
-    .ok_or(TRCError::Unknown(
-      "Failed to find first image in document".to_owned(),
+    .exists()
+    {
+        return Ok(());
+    }
+
+    let mut search_response = reqwest::get(&format!(
+        "https://www.google.com/search?q={}&tbm=isch&tbs=ift:jpg",
+        word
     ))?;
-  let mut source = reqwest::get(url)?;
 
-  if !Path::new(&format!("./static/images/{}", language_abbr)).exists() {
-    create_dir(&format!("./static/images/{}", language_abbr))?;
-  }
+    let text = search_response.text()?;
+    let document = Document::from_read(::std::io::Cursor::new(text.into_bytes()))?;
+    let url = document
+        .find(And(Attr("alt", ""), Name("img")))
+        .filter_map(|n| n.attr("src"))
+        .nth(0)
+        .ok_or(TRCError::Unknown(
+            "Failed to find first image in document".to_owned(),
+        ))?;
+    let mut source = reqwest::get(url)?;
 
-  let mut dest = File::create(format!(
-    "./static/images/{}/{}.jpg",
-    language_abbr, sanitized
-  ))?;
+    if !Path::new(&format!("./static/images/{}", language_abbr)).exists() {
+        create_dir(&format!("./static/images/{}", language_abbr))?;
+    }
 
-  copy(&mut source, &mut dest)?;
+    let mut dest = File::create(format!(
+        "./static/images/{}/{}.jpg",
+        language_abbr, sanitized
+    ))?;
 
-  Ok(())
+    copy(&mut source, &mut dest)?;
+
+    Ok(())
 }
 
 pub fn get_audio_from_google(
-  language_abbr: &str,
-  word: &str,
-  sanitized: &str,
+    language_abbr: &str,
+    word: &str,
+    sanitized: &str,
 ) -> Result<(), TRCError> {
-  if Path::new(&format!(
-    "./static/audio/{}/{}.mp3",
-    language_abbr, sanitized
-  ))
-  .exists()
-  {
-    return Ok(());
-  }
+    if Path::new(&format!(
+        "./static/audio/{}/{}.mp3",
+        language_abbr, sanitized
+    ))
+    .exists()
+    {
+        return Ok(());
+    }
 
-  let url = google_translate_tts::url(word, language_abbr);
-  let mut source = reqwest::get(&url)?;
+    let url = google_translate_tts::url(word, language_abbr);
+    let mut source = reqwest::get(&url)?;
 
-  if !Path::new(&format!("./static/audio/{}", language_abbr)).exists() {
-    create_dir(&format!("./static/audio/{}", language_abbr))?;
-  }
+    if !Path::new(&format!("./static/audio/{}", language_abbr)).exists() {
+        create_dir(&format!("./static/audio/{}", language_abbr))?;
+    }
 
-  let mut dest = File::create(format!(
-    "./static/audio/{}/{}.mp3",
-    language_abbr, sanitized
-  ))?;
+    let mut dest = File::create(format!(
+        "./static/audio/{}/{}.mp3",
+        language_abbr, sanitized
+    ))?;
 
-  copy(&mut source, &mut dest)?;
+    copy(&mut source, &mut dest)?;
 
-  Ok(())
+    Ok(())
 }
